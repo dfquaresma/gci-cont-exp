4aa369d8aba212091b8fb2f822ebeb429d3152dd
dfquaresma <david.quaresma@ccc.ufcg.edu.br>
2019-05-11 15:31:49 -0300

    Update lb-vm-setup

--- setup/lb-vm-setup.sh
+++ setup/lb-vm-setup.sh
@@ -19,7 +19,7 @@ update-rc.d haveged defaults
 # Install Nginx 
 #add-apt-repository ppa:nginx/stable -y; apt-get update -y # Needed to install on ubuntu 14.
 apt-get install nginx -y
-wget http://www2.lsd.ufcg.edu.br/~davidfq/gci-setup/nginx.conf
+wget https://raw.githubusercontent.com/dfquaresma/socc19/master/setup/nginx.conf
 mv nginx.conf /etc/nginx/
 
 # Download YCSB
@@ -27,6 +27,8 @@ git clone https://github.com/dfquaresma/YCSB
 # Download dependencies
 apt install -y maven
 apt install -y python
+wget https://raw.githubusercontent.com/dfquaresma/socc19/master/setup/rest_workload
+mv rest_workload workloads/
 # Compile only what is needed.
 cd YCSB
 mvn -pl com.yahoo.ycsb:rest-binding -am clean package
