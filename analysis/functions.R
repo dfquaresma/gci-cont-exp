b0818fc730aaf691c42ea6f0828c2ba6b9531cb5
dfquaresma <david.quaresma@ccc.ufcg.edu.br>
2019-05-12 20:57:19 -0300

    Refact analysis scripts

--- analysis/functions.R
+++ analysis/functions.R
@@ -6,9 +6,13 @@ require(dplyr)
 # gcierr: número de requisições que falharam no GCI
 # zeroreq: vetor ou lista de tempos de resposta das requisições com Zero Coletas
 # zeroerr: número de requisições que falharam no Zero Coletas
-print_summary_table <- function(nogcireq, nogcierr, gcireq, gcierr, zeroreq, zeroerr) {
-  cat("#fail:", "NG:", nogcierr, "\t | ", "G:", gcierr, "\t| ", "ZC:", zeroerr,  "\n")
-  cat("#req: ", "NG:", NROW(nogcireq), " | ", "G:", NROW(gcireq), "\t| ", "ZC:", NROW(zeroreq), "\n")
+table_it <- function(runtime_language, nogcireq, nogcierr, gcireq, gcierr, zeroreq, zeroerr) {
+  cat("#fail:", paste(runtime_language, "NG:", sep=""), nogcierr, " | ", 
+      paste(runtime_language, "G:", sep=""), gcierr, " | ", 
+      paste(runtime_language, "ZC:", sep=""), zeroerr,  "\n")
+  cat("#req: ", paste(runtime_language, "NG:", sep=""), NROW(nogcireq), " | ", 
+      paste(runtime_language, "G:", sep=""), NROW(gcireq), " | ", 
+      paste(runtime_language, "ZC:", sep=""), NROW(zeroreq), "\n")
   stats <- function(df, tag) {
     p50 = quantile(df, 0.5)
     p95 = quantile(df, 0.95)
@@ -28,7 +32,35 @@ print_summary_table <- function(nogcireq, nogcierr, gcireq, gcierr, zeroreq, zer
     cat("Dist.Tail.:", signif(p99999-p50, digits = 4)) 
     cat("\n")
   }
-  stats(nogcireq, "NG")
-  stats(gcireq, " G")
-  stats(zeroreq, "ZC")
+  stats(nogcireq, paste(runtime_language, "NG", sep=""))
+  stats(gcireq, paste(runtime_language, " G", sep=""))
+  stats(zeroreq, paste(runtime_language, "ZC", sep=""))
+}
+
+print_summary_table <- function(path, runtime_language, experiment, round) {
+  nogci_path = paste(path, runtime_language, "nogci-access-e", experiment, "r", round, ".log", sep="")
+  gci_path = paste(path, runtime_language, "gci-access-e", experiment, "r", round, ".log", sep="")
+  zero_path = paste(path, runtime_language, "zero-access-e", experiment, "r", round, ".log", sep="")
+    
+  nogci = read.csv(nogci_path, sep=";",header=T, dec=".")
+  nogci = tail(gonogci, -100)
+  nogci.req <- as.vector(filter(gonogci, status == 200)$request_time) * 1000
+  nogci.err <- nrow(filter(gonogci, status != 200))
+  
+  gci = read.csv(gci_path, sep=";",header=T, dec=".")
+  gci = tail(gogci, -100)
+  gci.req <- as.vector(filter(gogci, status == 200)$request_time) * 1000
+  gci.err <- nrow(filter(gogci, status != 200))
+  
+  zero = read.csv(zero_path, sep=";",header=T, dec=".")
+  zero = tail(gozero, -100)
+  zero.req <- as.vector(filter(gozero, status == 200)$request_time) * 1000
+  zero.err <- nrow(filter(gozero, status != 200))
+  
+  table_it(
+    runtime_language,
+    nogci.req, nogci.err, 
+    gci.req, gci.err, 
+    zero.req, zero.err
+  )  
 }
