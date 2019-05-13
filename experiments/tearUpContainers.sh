01d84cc5c4f9cb28362e66bb81980a238bea0721
dfquaresma <david.quaresma@ccc.ufcg.edu.br>
2019-05-13 17:39:27 -0300

    Add script to tear up and down containers

---
+++ experiments/tearUpContainers.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+date
+set -x
+
+LAST_IP_NUMBER="198 218 231 242"
+COMMAND="sudo docker run --network=host --rm --name container-${CONTAINER_TAG} image-${CONTAINER_TAG}"
+for i in ${LAST_IP_NUMBER}
+do
+	ssh-keygen -f "/home/ubuntu/.ssh/known_hosts" -R 10.11.4.${i}
+	ssh -i id_rsa  ubuntu@10.11.4.${i} -o StrictHostKeyChecking=no ${COMMAND}
+done
