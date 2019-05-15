eb799776923cd84971915b02fc24c4ec402fd888
dfquaresma <david.quaresma@ccc.ufcg.edu.br>
2019-05-15 11:41:39 -0300

    Update tearDown and setUp containers

--- experiments/setUpContainers.sh
+++ experiments/setUpContainers.sh
@@ -7,6 +7,6 @@ COMMAND="sudo docker run --network=host --rm --name container-${CONTAINER_TAG} i
 for i in ${LAST_IP_NUMBER}
 do
 	ssh-keygen -f "/home/ubuntu/.ssh/known_hosts" -R 10.11.4.${i}
-	ssh -i id_rsa  ubuntu@10.11.4.${i} -o StrictHostKeyChecking=no ${COMMAND}
+	ssh -i id_rsa  ubuntu@10.11.4.${i} -o StrictHostKeyChecking=no "${COMMAND}"
 done
 sleep 10
