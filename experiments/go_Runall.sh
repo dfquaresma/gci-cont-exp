99421909998c79948028e0cb012d09902661ee3f
dfquaresma <david.quaresma@ccc.ufcg.edu.br>
2019-05-16 16:36:42 -0300

    Add go run all script

---
+++ experiments/go_Runall.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+date
+set -x
+
+RED='\033[0;31m'
+NC='\033[0m'
+
+echo "ID_RSA_PATH: ${ID_RSA_PATH:=../../id_rsa}"
+echo "RESULTS_PATH: ${RESULTS_PATH:=../analysis/results/}"
+echo "EXPID_START: ${EXPID_START:=1}"
+echo "EXPID_END: ${EXPID_END:=3}"
+echo "GOTAGS: ${GOTAGS:=gogci gonogci gozero}"
+
+for tag in ${GOTAGS};
+do
+    for expid in `seq ${EXPID_START} ${EXPID_END}`
+    do
+        echo ""
+        echo -e "${RED}EXPID ${expid}!${NC}"
+        ID_RSA_PATH=${ID_RSA_PATH} RESULTS_PATH=${RESULTS_PATH} CONTAINER_TAG=${tag} EXPID=${expid}
+    done
+done
