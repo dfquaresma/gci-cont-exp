3bafb0f4b7cd0d5f857fe8ddf48d88117fa653c6
dfquaresma <david.quaresma@ccc.ufcg.edu.br>
2019-05-20 14:58:06 -0300

    Update workload script to remove old ycsb results

--- experiments/workload.sh
+++ experiments/workload.sh
@@ -3,6 +3,6 @@ date
 set -x
 
 ssh-keygen -f "/home/ubuntu/.ssh/known_hosts" -R 10.11.4.113
-ssh -i ${ID_RSA_PATH}  ubuntu@10.11.4.113 -o StrictHostKeyChecking=no "sudo rm /var/log/nginx/*.log; sudo service nginx restart"
+ssh -i ${ID_RSA_PATH}  ubuntu@10.11.4.113 -o StrictHostKeyChecking=no "sudo rm /var/log/nginx/*.log; sudo service nginx restart; sudo rm -rf /home/ubuntu/*-YCSB-std*-e*.log"
 sleep 5
 ssh -i ${ID_RSA_PATH}  ubuntu@10.11.4.113 -o StrictHostKeyChecking=no "cd /home/ubuntu/YCSB/; ./bin/ycsb run rest -s -P workloads/rest_workload >/home/ubuntu/${CONTAINER_TAG}-YCSB-stdout-e${EXPID}.log 2>/home/ubuntu/${CONTAINER_TAG}-YCSB-stderr-e${EXPID}.log"
