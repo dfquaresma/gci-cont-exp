bf26cffc10ffec0d066003607f65bf8f1c16dd91
dfquaresma <david.quaresma@ccc.ufcg.edu.br>
2019-05-20 10:17:35 -0300

    Fix container build at container's setup script

--- setup/container-vm-setup.sh
+++ setup/container-vm-setup.sh
@@ -21,12 +21,16 @@ apt -y install docker.io
 systemctl start docker
 systemctl enable docker
 
-# Download experiment's repository
+# Clone experiment's repository
+apt -y install golang-go
 git clone https://github.com/dfquaresma/socc19
 chown -R ubuntu socc19/
 cd /home/ubuntu/socc19/containers/go-gci
+gofmt -s -w main; gofmt -s -w function/handler.go
 docker build -t image-gogci . 
 cd /home/ubuntu/socc19/containers/go-nogci
+gofmt -s -w main; gofmt -s -w function/handler.go
 docker build -t image-gonogci . 
 cd /home/ubuntu/socc19/containers/go-zero
+gofmt -s -w main; gofmt -s -w function/handler.go
 docker build -t image-gozero . 
