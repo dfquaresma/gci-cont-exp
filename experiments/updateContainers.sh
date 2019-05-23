cb8864c4dbfd7ed05b2626fb22da616122053e3a
David Ferreira Quaresma <davidferreiraquaresma@gmail.com>
2019-05-23 03:00:54 -0300

    Update updateContainers.sh

--- experiments/updateContainers.sh
+++ experiments/updateContainers.sh
@@ -10,7 +10,7 @@ then
 fi
 
 LAST_IP_NUMBER="198 218 231 242"
-COMMAND="bash /home/ubuntu/socc19/setup/update-container.sh"
+COMMAND="sudo bash /home/ubuntu/socc19/setup/update-container.sh"
 for i in ${LAST_IP_NUMBER}
 do
 	ssh-keygen -f "/home/ubuntu/.ssh/known_hosts" -R 10.11.4.${i}
