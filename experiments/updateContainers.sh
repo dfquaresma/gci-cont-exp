b34dffdc93457975a420c9b714853ee8b24c54fa
dfquaresma <david.quaresma@ccc.ufcg.edu.br>
2019-05-23 14:52:18 -0300

    Fix updateContainers.sh

--- experiments/updateContainers.sh
+++ experiments/updateContainers.sh
@@ -8,11 +8,11 @@ echo "ID_RSA_PATH: ${ID_RSA_PATH:=../../id_rsa}"
 ssh -i ${ID_RSA_PATH}  ubuntu@10.11.4.113 -o StrictHostKeyChecking=no "rm -rf /home/ubuntu/YCSB/workload/rest_workload; curl -sSL https://raw.githubusercontent.com/dfquaresma/socc19/master/setup/rest_workload > /home/ubuntu/YCSB/workloads/rest_workload"
 
 LAST_IP_NUMBER="198 218 231 242"
-DELETE_COMMAND="sudo docker rmi -f $(sudo docker images -q)"
+DELETE_COMMAND='sudo docker rmi -f $(sudo docker images -q)'
 UPDATE_COMMAND="sudo bash /home/ubuntu/socc19/setup/update-container.sh"
 for i in ${LAST_IP_NUMBER}
 do
 	ssh-keygen -f "/home/ubuntu/.ssh/known_hosts" -R 10.11.4.${i}
-	ssh -i ${ID_RSA_PATH}  ubuntu@10.11.4.${i} -o StrictHostKeyChecking=no '${DELETE_COMMAND}'
+	ssh -i ${ID_RSA_PATH}  ubuntu@10.11.4.${i} -o StrictHostKeyChecking=no "${DELETE_COMMAND}"
 	ssh -i ${ID_RSA_PATH}  ubuntu@10.11.4.${i} -o StrictHostKeyChecking=no "${UPDATE_COMMAND}"
 done
