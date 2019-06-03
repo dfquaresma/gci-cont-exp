e88dea15a3cef6145e8e11a1bee1b7ccc19f8e97
David Ferreira Quaresma <davidferreiraquaresma@gmail.com>
2019-06-03 15:04:42 -0300

    Update setUpContainers.sh

--- experiments/setUpContainers.sh
+++ experiments/setUpContainers.sh
@@ -10,8 +10,8 @@ then
 fi
 
 LAST_IP_NUMBER="198 218 231 242"
-#COMMAND="sudo docker run -d --network=host --cpus=1.0 --memory=${CONTAINER_MEM_LIM} --memory-swap=${CONTAINER_MEM_LIM} --rm --name container-${CONTAINER_TAG} image-${CONTAINER_TAG}"
-COMMAND="sudo docker run -d --network=host --cpus=1.0 --rm --name container-${CONTAINER_TAG} image-${CONTAINER_TAG}"
+#COMMAND="sudo docker run -d --network=host --cpus=1.0 --cpuset-cpus=0 --memory=${CONTAINER_MEM_LIM} --memory-swap=${CONTAINER_MEM_LIM} --rm --name container-${CONTAINER_TAG} image-${CONTAINER_TAG}"
+COMMAND="sudo docker run -d --network=host --cpus=1.0 --cpuset-cpus=0 --rm --name container-${CONTAINER_TAG} image-${CONTAINER_TAG}"
 for i in ${LAST_IP_NUMBER}
 do
 	ssh-keygen -f "/home/ubuntu/.ssh/known_hosts" -R 10.11.4.${i}
