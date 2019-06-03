ed2f7e1d2a9fce16940eda284385d1764eb77926
David Ferreira Quaresma <davidferreiraquaresma@gmail.com>
2019-06-03 16:08:36 -0300

    Update update-container.sh

--- setup/update-container.sh
+++ setup/update-container.sh
@@ -11,11 +11,11 @@ cd /home/ubuntu/
 # Update containers images
 cd /home/ubuntu/socc19/containers/go-gci
 gofmt -s -w main; gofmt -s -w function/handler.go
-docker build -t image-gogci . 
+docker build -t image-gogci .
 cd /home/ubuntu/socc19/containers/go-nogci
 gofmt -s -w main; gofmt -s -w function/handler.go
-docker build -t image-gonogci . 
+docker build -t image-gonogci .
 cd /home/ubuntu/socc19/containers/go-zero
 gofmt -s -w main; gofmt -s -w function/handler.go
-docker build -t image-gozero . 
+docker build -t image-gozero .
 cd /home/ubuntu/
